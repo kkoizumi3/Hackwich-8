87f0a7095e37dc0740d019ed79f272dc2994108d
Kaci Koizumi <koizumi3@hawaii.edu>
2023-10-19 13:19:15 -1000

    did prob set 2

--- Hackwich 8/ViewController.swift
+++ Hackwich 8/ViewController.swift
@@ -8,35 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     @IBOutlet weak var mylabel: UILabel!
     
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
-        mylabel.text = "My Segmented Control"
         
+        // mylabel.text = "My Segmented Control"
+        
+        //for prob set 2
+        mylabel.text = ""
+        segmentedControl.selectedSegmentIndex = -1
     }
-
+    
     @IBAction func segmentedControlPressed(_ sender: Any)
     {
+        /*
+         switch segmentedControl.selectedSegmentIndex
+         {
+         case 0: mylabel.text = "First Segment has been selected"
+         
+         case 1: mylabel.text = "Second Segment has been selected"
+         
+         case 2: mylabel.text = "Woohoo, this makes sense now"
+         
+         default:break
+         
+         }
+         */
+        
+        //prob set 2
         
         switch segmentedControl.selectedSegmentIndex
-            {
-        case 0: mylabel.text = "First Segment has been selected"
+        {
+        case 0: mylabel.text = textField.text
             
-            case 1: mylabel.text = "Second Segment has been selected"
+        case 1: mylabel.text = textField.text
             
-        case 2: mylabel.text = "Woohoo, this makes sense now"
-               
-            default:break
+        case 2: mylabel.text = textField.text
+            
+        default:break
+            
+            
         }
-
         
     }
     
 }
-
